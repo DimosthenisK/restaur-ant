81043140e266ec3c9cd972620910049a220861b4
Dimosthenis Kalaitzis <dimostheniskalaitzis@gmail.com>
2022-01-09 12:24:04 +0200

    Add Reviews

--- restaur-ant-backend/src/restaurant/review/review.controller.ts
+++ restaur-ant-backend/src/restaurant/review/review.controller.ts
@@ -1,7 +1,110 @@
-import { Controller } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiParam,
+  ApiTags
+  } from '@nestjs/swagger';
+import { Review, User } from '@prisma/client';
+import { Request as ExpressRequest } from 'express';
 import { ReviewService } from './review.service';
-
+import { Roles } from '../../user/authentication/decorators/roles.decorator';
+import { BearerAuthGuard } from '../../user/authentication/guards/bearer.guard';
+import { RolesGuard } from '../../user/authentication/guards/roles.guard';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  CreateReviewDto,
+  UpdateReviewDto,
+} from '../..//generated/dto/review/dto';
+@ApiTags('Review')
+@ApiBearerAuth('bearer')
+@UseGuards(BearerAuthGuard)
 @Controller()
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
+
+  @ApiParam({ name: 'restaurantId', type: 'string' })
+  @Get('')
+  async findAll(@Param('restaurantId') restaurantId: string): Promise<any> {
+    return this.reviewService.findForRestaurant(restaurantId);
+  }
+
+  @Post()
+  @ApiBody({ type: () => CreateReviewDto })
+  @ApiParam({ name: 'restaurantId', type: 'string' })
+  async create(
+    @Body() dto: Partial<Review>,
+    @Param('restaurantId') restaurantId: string,
+    @Request() request: ExpressRequest & { user: User },
+  ) {
+    const userId = request.user.id;
+    try {
+      console.log(restaurantId, userId, dto);
+      const newReview = await this.reviewService.create(
+        userId,
+        restaurantId,
+        dto,
+      );
+
+      return {
+        success: true,
+        message: 'Review created successfully',
+        data: newReview.id,
+      };
+    } catch (err) {
+      //TODO Handle
+      console.log(err);
+      return { success: false, message: err.message };
+    }
+  }
+
+  @UseGuards(RolesGuard)
+  @Roles('ADMIN')
+  @Patch('/:id')
+  @ApiParam({ name: 'id', type: 'string' })
+  @ApiBody({ type: () => UpdateReviewDto })
+  async update(@Body() dto: Partial<Review>, @Param('id') id: string) {
+    try {
+      const updatedReview = await this.reviewService.update(id, dto);
+
+      return {
+        success: true,
+        message: 'Review updated successfully',
+        data: updatedReview.id,
+      };
+    } catch (err) {
+      //TODO Handle
+      console.log(err);
+      return { success: false, message: err.message };
+    }
+  }
+
+  @UseGuards(RolesGuard)
+  @Roles('ADMIN')
+  @Delete('/:id')
+  @ApiParam({ name: 'id', type: 'string' })
+  async delete(@Param('id') id: string) {
+    try {
+      const deletedReview = await this.reviewService.delete(id);
+
+      return {
+        success: true,
+        message: 'Review deleted successfully',
+        data: deletedReview.id,
+      };
+    } catch (err) {
+      //TODO Handle
+      console.log(err);
+      return { success: false, message: err.message };
+    }
+  }
 }
--- restaur-ant-backend/src/restaurant/review/review.service.ts
+++ restaur-ant-backend/src/restaurant/review/review.service.ts
@@ -1,4 +1,138 @@
 import { Injectable } from '@nestjs/common';
+import { Review, ReviewStatus } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class ReviewService {}
+export class ReviewService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async findForRestaurant(restaurantId: string) {
+    const getAverageRating = async () => {
+      const reviews = await this.prismaService.review.aggregate({
+        _avg: {
+          rating: true,
+        },
+        where: {
+          status: {
+            equals: ReviewStatus.PUBLISHED,
+          },
+          restaurantId: { equals: restaurantId },
+        },
+      });
+
+      return reviews._avg.rating;
+    };
+    const getHighestRated = async () => {
+      return this.prismaService.review.findFirst({
+        where: {
+          status: {
+            equals: ReviewStatus.PUBLISHED,
+          },
+          restaurantId: { equals: restaurantId },
+        },
+        orderBy: [{ rating: 'desc' }],
+      });
+    };
+    const getLowestRated = async () => {
+      return this.prismaService.review.findFirst({
+        where: {
+          status: {
+            equals: ReviewStatus.PUBLISHED,
+          },
+          restaurantId: { equals: restaurantId },
+        },
+        orderBy: [{ rating: 'asc' }],
+      });
+    };
+    const getLatest = async () => {
+      return this.prismaService.review.findFirst({
+        where: {
+          status: {
+            equals: ReviewStatus.PUBLISHED,
+          },
+          restaurantId: { equals: restaurantId },
+        },
+        orderBy: [{ createdAt: 'desc' }],
+      });
+    };
+
+    return {
+      averageRating: await getAverageRating(),
+      highestRated: await getHighestRated(),
+      lowestRated: await getLowestRated(),
+      latest: await getLatest(),
+    };
+  }
+
+  async create(
+    userId: string,
+    restaurantId: string,
+    reviewDto: Partial<Review>,
+  ) {
+    const review: Review = {
+      id: undefined,
+      userId,
+      restaurantId,
+      rating: reviewDto.rating,
+      comment: reviewDto.comment,
+      dateOfVisit: reviewDto.dateOfVisit,
+      status: undefined,
+      createdAt: undefined,
+    };
+
+    try {
+      const createdReview = await this.prismaService.review.create({
+        data: review,
+      });
+      return createdReview;
+    } catch (err) {
+      //TODO HANDLE
+      throw err;
+    }
+  }
+
+  async update(id: string, reviewDto: Partial<Review>) {
+    const review = await this.prismaService.review.findUnique({
+      where: { id },
+    });
+
+    if (review) {
+      const sanitaryReview: Review = {
+        ...review,
+        rating: reviewDto.rating,
+        comment: reviewDto.comment,
+        dateOfVisit: reviewDto.dateOfVisit,
+      };
+
+      try {
+        const updatedReview = await this.prismaService.review.update({
+          data: sanitaryReview,
+          where: { id },
+        });
+        return updatedReview;
+      } catch (err) {
+        //TODO HANDLE
+        throw err;
+      }
+    } else throw new Error(`REVIEW_NOT_FOUND`);
+  }
+
+  async delete(id: string) {
+    const review = await this.prismaService.review.findUnique({
+      where: { id },
+    });
+
+    if (review) {
+      try {
+        const updatedReview = await this.prismaService.review.update({
+          data: { status: ReviewStatus.DELETED },
+          where: { id },
+        });
+        return updatedReview;
+      } catch (err) {
+        //TODO HANDLE
+        throw err;
+      }
+    } else throw new Error(`REVIEW_NOT_FOUND`);
+  }
+}
